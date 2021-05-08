a948398fb08d843132668a178364bbfff58f732d
Islam <78363252+aibekovislam@users.noreply.github.com>
2021-05-08 17:41:37 +0300

    modified models

--- core/models.py
+++ core/models.py
@@ -8,11 +8,19 @@ class AboutUs(models.Model):
     text = models.TextField() 
     img = models.ImageField(upload_to="AboutUs_images", blank=True, null=True, verbose_name="Картинки 'О нас'")
 
+    class Meta:
+        verbose_name = "О нас"
+        verbose_name_plural = "О нас"
+
 
 class TeamCodifyLab(models.Model):
     name = models.CharField(max_length=255)
     descriptions = models.TextField()
     img_team = models.ImageField(upload_to="TeamsImg", blank=True, null=True, verbose_name="Картинки команды CodifyLab")
+
+    class Meta:
+        verbose_name = "Команда CodifyLab"
+        verbose_name_plural = "Команды CodifyLab"
 
 
 
@@ -25,17 +33,29 @@ class ContanctsCodifyLab(models.Model):
     twitter = models.CharField(max_length=55)
     facebook = models.CharField(max_length=55)
 
+    class Meta:
+        verbose_name = "Контакт"
+        verbose_name_plural = "Контакты"
+
 
 class Plus(models.Model):
     title = models.CharField(max_length=255)
     text = models.TextField()
     img_plus = models.ImageField(upload_to="Plus_img", blank=True, null=True, verbose_name="Картинки Plus")
 
+    class Meta:
+        verbose_name = "Преимущество"
+        verbose_name_plural = "Преимущества"
+
 
 class Franchise(models.Model):
     name = models.CharField(max_length=250)
     text = models.TextField()
     img_franchise = models.ImageField(upload_to="Franchise_img", blank=True, null=True, verbose_name="Картинки франишизы")
+
+    class Meta:
+        verbose_name = "Франшиза"
+        verbose_name_plural = "Франшизы"
 
 
 class Application(models.Model):
@@ -44,6 +64,10 @@ class Application(models.Model):
     mail = models.EmailField(max_length=254)
     phone_number = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
+
+    class Meta:
+        verbose_name = "Форма отправки"
+        verbose_name_plural = "Форма отправки"
 
 
 class Feedback(models.Model):
@@ -54,8 +78,16 @@ class Feedback(models.Model):
     phone_number = models.CharField(max_length=20)
     created_at = models.DateTimeField(auto_now_add=True, null=True)
 
+    class Meta:
+        verbose_name = "Отзыв"
+        verbose_name_plural = "Отзывы"
+
 
 class FAQ(models.Model):
     question = models.TextField()
     answer = models.TextField()
+
+    class Meta:
+        verbose_name = "Вопрос"
+        verbose_name_plural = "Вопросы"
 
